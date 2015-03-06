1fdc4ebb68b39258405e62878cf371cd48733532
Vikram Narayan <vnaray1@gmail.com>
2015-03-06 00:39:40 -0500

    modified corpus

--- corpus.py
+++ corpus.py
@@ -13,8 +13,31 @@ def write_to_file(transposed_chorale, file_name):
 	f.write('(')
 	for item in transposed_chorale:
 		if type(item)==note.Note:
-			ns = str(item).split(' ')[1].split('>')[0]
-			f.write(' '+ns)
+			note_val = str(item).split(' ')[1].split('>')[0]
+			print note_val
+			print item
+			if note_val.quarterLength==1:
+				# if the note is a quarter note, just write it
+				f.write(' '+note_val)
+			elif note_val.quarterLength>1:
+				# if the note is more than a quarter note, write in the
+				# number of quarter notes that could fit in the note
+				temp=note_val.quarterLength
+				while temp >= 1:
+					f.write(' '+note_val)
+					temp-=1
+
+			else: # if the note is less than a quarter note
+				# if item.next():
+				# 	# if the note is less than a quarter note and 
+				# 	if item.next().quarterLength < 1:
+				# 		f.write(' '+note_val)
+				# 		continue
+				# 	else:
+				# 		f.write(' '+note_val)
+				# else:
+				# 	f.write(' '+note_val)
+				f.write(' '+note_val)
 
 	f.write(')\n')
 	f.close()
