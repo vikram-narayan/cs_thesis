2da55e687fac3113caffe5f0e294870bc98adba1
Vikram Narayan <vnaray1@gmail.com>
2015-03-10 01:01:31 -0400

    added forward and backward algorithms

--- train.py
+++ train.py
@@ -128,10 +128,99 @@ class HMM:
         while len(stack)>0:
             viterbi_path2.append(stack.pop())
 
-        # -- TODO end
         return (viterbi_path,viterbi_path2)
 
+    def forward_algorithm(self, observation):
+        """given an observation as a list of symbols,
+        run the forward algorithm"""
 
+        # initialize forward algorithm table
+        fwd_table={}
+        fwd_table['scaling factor']=[]
+        for i in range(len(observation)):
+            fwd_table['scaling factor'].append(0)
+
+        for state in self.states:
+            fwd_table[state]=[]
+            for i in range(len(observation)):
+                fwd_table[state].append(0)
+
+        # initialize first col of fwd algorithm table
+        for state in self.states:
+            # logs will be taken at the end 
+            fwd_table[state][0] = (self.transitions[self.start][state] * self.emissions[state][observation[0]] ) 
+
+        # fill in the rest of the forward table
+        for output in range(1,len(observation)):
+            for state in self.states:
+                fwd=0
+                for prev_state in self.states:
+                    fwd+=fwd_table[prev_state][output-1] * self.transitions[prev_state][state] * self.emissions[state][observation[output]]
+                fwd_table[state][output] = fwd
+
+        return fwd_table
+
+    def total_probability(self, observation):
+        """compute the probability of the observation under the model"""
+        observation=observation.split()
+        fwd_table = self.forward_algorithm(observation)
+
+        # forward_prob = numpy.log10(numpy.prod(fwd_table['scaling factor']))
+
+        forward_prob=0
+        for state in self.states:
+            forward_prob+=fwd_table[state][len(observation)-1]
+
+        return numpy.log10(forward_prob)
+
+
+
+    def backward_algorithm(self, observation):
+        """given an observation as a list of symbols,
+        find the probability of the observation under this HMM,
+        using the backward algorithm"""
+        # initialize backward algorithm table
+        bk_table={}
+        bk_table['scaling factor']=[]
+        observation2 = [self.start]+observation
+        for i in range(len(observation2)):
+            bk_table['scaling factor'].append(0)
+
+        for state in self.states:
+            bk_table[state]=[]
+            for i in range(len(observation2)):
+                bk_table[state].append(0)
+
+        # initialize and scale last column
+        for state in self.states:
+            bk_table[state][len(observation2)-1]=1.0
+
+        output=len(observation2)-2
+        while output>=1:
+            for state in self.states:
+                back=0
+                for after_state in self.states:
+                    back+=self.transitions[state][after_state] * self.emissions[after_state][observation2[output+1]] * bk_table[after_state][output+1]
+                bk_table[state][output] = back
+
+            output=output-1
+
+        back=0
+        for state in self.states:
+            back+= self.transitions[self.start][state] * self.emissions[state][observation2[1]] * bk_table[state][1]
+        for state in self.states:
+            bk_table[state][0]=back
+
+        return bk_table
+
+    def total_probability_bk(self, observation):
+        """compute the probability of the observation under the model"""
+        observation=observation.split()
+        bk_table = self.backward_algorithm(observation)
+        for state in self.states:
+            bk_prob = bk_table[state][0]
+        
+        return numpy.log10(bk_prob)
 
 
 if __name__=='__main__':
@@ -160,4 +249,6 @@ if __name__=='__main__':
 	hierarchicalHMM.flatten()
 	hmm = HMM(hierarchicalHMM, 'bach_chorales_a4.data')
 	x=hmm.best_state_sequence(hmm.observations[340])
+	y=hmm.total_probability(hmm.observations[329])
+	z=hmm.total_probability_bk(hmm.observations[329])
 	pdb.set_trace()
